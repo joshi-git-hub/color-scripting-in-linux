1c36a25995590d301e554ce2e48e1861dc2f8020
joshi-git-hub <37043894+joshi-git-hub@users.noreply.github.com>
2018-03-28 02:42:44 +0530

    code is ready

---
+++ color.sh
@@ -0,0 +1,5 @@
+#this is for prnting text in color 
+#!/bin/bash
+echo "hello friends i am Ganesh."
+echo -e "\033[31;42;01m Linux is the future of IT professionals.\033[0m"
+echo -e "\033[31;41;01m Redhat is one of the best distribution of Linux.\033[0m"
